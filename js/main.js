a627213143a628c7a9902a6bbfb714e02f5b9a19
IgorGurskiy <profiigor35@gmail.com>
2021-07-14 17:11:32 +0300

    added reviews section & fixed hotel slider

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
-const swiper = new Swiper('.swiper-container', {
+const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button__next',
-    prevEl: '.slider-button__prev',
+    nextEl: '.hotel-slider__button__next',
+    prevEl: '.hotel-slider__button__prev',
   },
   effect: "coverflow",
+
+});
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button__next',
+    prevEl: '.reviews-slider__button__prev',
+  },
 
 });
